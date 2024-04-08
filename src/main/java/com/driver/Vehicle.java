a93c8829ccb837f0fb189b49145c29dc8aaab1d6
nagaratnashetty <125356022+nagaratnashetty@users.noreply.github.com>
2024-04-08 19:06:10 +0530

    Update Vehicle.java

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -7,34 +7,41 @@ public class Vehicle {
     private int currentDirection;
 
     public Vehicle(String name) {
-        this.name = name;
+        this.name=name;
+        this.currentSpeed=0;
+        this.currentDirection=0;
+
     }
 
+
+
     public void steer(int direction){
-        currentDirection += direction;
+        //direction is in degrees, add it to the current direction
+          this.currentDirection+=direction;
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
     public void move(int speed, int direction){
-        currentSpeed = speed;
-        currentDirection = direction;
+        //set the values of currentSpeed and currentDirection
+        this.currentSpeed=speed;
+        this.currentDirection=direction;
         System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
     }
 
     public void stop(){
-        currentSpeed = 0;
+         this.currentSpeed=0;
         System.out.println("stop method called - The vehicle is stopped");
     }
-
-    public String getName() {
-        return name;
+    public int getCurrentDirection() {
+        return this.currentDirection;
     }
 
     public int getCurrentSpeed() {
-        return currentSpeed;
+        return this.currentSpeed;
     }
 
-    public int getCurrentDirection() {
-        return currentDirection;
+    public String getName() {
+        return this.name;
     }
+
 }
